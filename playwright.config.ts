f3b02d3093ecb2fef4c54c4483557319d121e477
Alex Kuzmenkov <alex.kuzmenkov@yandex.com>
2026-07-17 12:54:40 +0200

    fix for video+slowMo delays

--- playwright.config.ts
+++ playwright.config.ts
@@ -30,6 +30,11 @@ export default defineConfig({
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
+    video: 'on-first-retry',
+    launchOptions: {
+      headless: false,
+      slowMo: 300,
+    },
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
 
